b4c0cc7258012772b53e267af242b26c482af330
Akshay KM <akshaykm194@gmail.com>
2021-12-17 01:39:47 +0530

    final file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
 function App() {
+  let [count,setCount]=useState(0)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="body">
+        <div className="rectangle">
+         <div className='line3'>
+
+         </div>
+         <div className='line2'>
+
+         </div>
+         <div className='line1'>
+
+         </div>
+         <div className='text-0'>
+           <h4>{count}</h4>
+         </div>
+         <div className='increment' onClick={()=>setCount(count+1)}>
+          <h6>+</h6>
+         </div>
+         <div className='decrement'onClick={()=>setCount(count-1)}>
+         <h6>-</h6>
+         </div>
+         <div className='heading'>
+          <h3>The Counter</h3>
+         </div>
+         <div className='discription'>
+          <p>Click the plus or minus buttons to add or subtract from the the number shown just above it </p>
+         </div>
+         <div className='reset'onClick={()=>setCount(count=0)}>
+          <h3>Reset Counter to 0</h3>
+         </div>
+        </div>
+
+      </div>
     </div>
   );
 }
